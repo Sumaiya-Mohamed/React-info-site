4247649522dd3a26c124647c39b3a7f174000320
unknown <sumimaya13@gmail.com>
2023-07-08 21:29:57 +0200

    changes

---
+++ src/components/Body.jsx
@@ -0,0 +1,16 @@
+
+export function Body(){
+    return(
+        <div className="body__container">
+            <h1>Fun facts about React:</h1>
+            <ul>
+                <li>Was first released in 2013.</li>
+                <li>Was originally created by Jordan Walke.</li>
+                <li>Has Well over 100k stars on github.</li>
+                <li>Is maintained by Facebook.</li>
+                <li>Powers thousands of enterprise apps,including mobile apps.</li>
+
+            </ul>
+        </div>
+    )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export function Header(){
+    return(
+        <div className="header__container">
+            <img src="./src/images/react.svg" alt="React Logo"></img>
+            <h1>React Facts</h1>
+        </div>
+    )
+}
